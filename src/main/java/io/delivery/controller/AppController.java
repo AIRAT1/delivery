896498da022c888e3ac1b98df694921cc7606bda
AIRAT1 <ayrat1@mail.ru>
2020-01-27 20:50:44 +0300

    add DB auth properties

--- src/main/java/io/delivery/controller/AppController.java
+++ src/main/java/io/delivery/controller/AppController.java
@@ -2,12 +2,15 @@ package io.delivery.controller;
 
 import io.delivery.model.Answer;
 import io.delivery.model.Message;
-import io.delivery.model.TableCreator;
 import io.delivery.service.CreateTable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
@@ -38,4 +41,13 @@ public class AppController {
     public String secure() {
         return "secure";
     }
+
+    @RequestMapping(value = {"/password/{password}"}, method = RequestMethod.GET)
+    public ModelAndView passwordEncode(@PathVariable("password") String password) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("password");
+        modelAndView.addObject("crypt", new BCryptPasswordEncoder().encode(password));
+        return modelAndView;
+    }
+
 }
---
+++ web/WEB-INF/views/password.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>Passport for user</h1>
+    <p>${crypt}</p>
+    <p>${password}</p>
+</body>
+</html>
