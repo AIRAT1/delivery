25e585d2605e39b059949cd4d71397b2a51d8c00
AIRAT1 <ayrat1@mail.ru>
2020-02-06 23:41:00 +0300

    add @OneToMany table dependency

--- src/main/java/io/delivery/entity/Document.java
+++ src/main/java/io/delivery/entity/Document.java
@@ -3,6 +3,8 @@ package io.delivery.entity;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "documents")
@@ -15,8 +17,22 @@ public class Document {
     @Column(name = "document_id")
     private long id;
 
-//    @Column(name = "document_name")
+    @Column(name = "document_name")
     private String name;
+
+    @Transient
+    private String specificInnerInfo;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private List<DocumentItem> documentItems = new ArrayList<>();
+
+    public List<DocumentItem> getDocumentItems() {
+        return documentItems;
+    }
+
+    public void setDocumentItems(List<DocumentItem> documentItems) {
+        this.documentItems = documentItems;
+    }
 
     public String getSpecificInnerInfo() {
         return specificInnerInfo;
@@ -25,9 +41,6 @@ public class Document {
     public void setSpecificInnerInfo(String specificInnerInfo) {
         this.specificInnerInfo = specificInnerInfo;
     }
-
-    @Transient
-    private String specificInnerInfo;
 
     public String getName() {
         return name;
